5e14fcc993ef719d0a07db1c31257357f1690c97
ahmedalmqtrione-hash <ahmed@example.com>
2026-06-22 09:42:09 +0300

    تحديث التوكن الجديد (حل الحظر)

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = "8813296918:AAEqwbdviGYh1TIZGdAaBWE0OldoZnPm8Mg"
+TOKEN = "8813296918:AAHuUdc2sfP2FjrfYZIufTWAYlvUstTr9N8"
 DEVELOPER_NAME = "أحمد حمدي أحمد عثمان المقطري"
 UNIVERSITY_NAME = "كلية الحاسبات وتقنية المعلومات"
 BOT_NAME = "النظام الخارق النهائي V-Final"
