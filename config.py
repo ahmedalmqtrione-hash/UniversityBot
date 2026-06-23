a68bb5aa794c9941af650a597a069e347b4b0404
ahmedalmqtrione-hash <ahmed@example.com>
2026-06-23 15:15:23 +0300

    تحديث التوكن الجديد لإزالة التقييد

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = "8813296918:AAHuUdc2sfP2FjrfYZIufTWAYlvUstTr9N8"
+TOKEN = "8813296918:AAH6lsUVrY9C4w8EN0hPfkEHlU8jf2EymSA"
 DEVELOPER_NAME = "أحمد حمدي أحمد عثمان المقطري"
 UNIVERSITY_NAME = "كلية الحاسبات وتقنية المعلومات"
 BOT_NAME = "النظام الخارق النهائي V-Final"
